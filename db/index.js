72ef84156393101d02a97ff0d13d72733506d43f
Lorena Gil <67319308+logiflo@users.noreply.github.com>
2023-05-04 11:55:16 +0100

    Add Group class.

--- db/index.js
+++ db/index.js
@@ -1,4 +1,13 @@
 const { User } = require('./User');
+const { Group } = require('./Group');
 const { sequelize, Sequelize } = require('./db');
 
-module.exports = { User, sequelize, Sequelize };
+User.hasMany(Group, { foreignKey: 'adminId' });
+Group.belongsTo(User, { foreignKey: 'adminId' });
+
+module.exports = {
+  User,
+  Group,
+  sequelize,
+  Sequelize,
+};
