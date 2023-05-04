eb91d7332405c08393d5c9b2d4d7217d5d300b60
Lorena Gil <67319308+logiflo@users.noreply.github.com>
2023-05-04 15:02:23 +0100

    Add Member class.

--- db/index.js
+++ db/index.js
@@ -1,13 +1,18 @@
 const { User } = require('./User');
 const { Group } = require('./Group');
+const { Member } = require('./Member');
 const { sequelize, Sequelize } = require('./db');
 
 User.hasMany(Group, { foreignKey: 'adminId' });
 Group.belongsTo(User, { foreignKey: 'adminId' });
 
+Group.hasMany(Member, { foreignKey: 'groupId' });
+Member.belongsTo(Group, { foreignKey: 'groupId' });
+
 module.exports = {
   User,
   Group,
+  Member,
   sequelize,
   Sequelize,
 };
