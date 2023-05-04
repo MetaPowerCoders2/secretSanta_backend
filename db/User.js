352da164444e561baa5f3b1a7a9ee908bbb058fa
Lorena Gil <67319308+logiflo@users.noreply.github.com>
2023-05-04 11:44:08 +0100

    Create User class

---
+++ db/User.js
@@ -0,0 +1,30 @@
+import { Sequelize, sequelize } from './db';
+
+const User = sequelize.define('user', {
+  id: {
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    allowNull: false,
+    primaryKey: true,
+    unique: true,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  mobile: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+export default { User };
