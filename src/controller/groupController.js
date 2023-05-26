2a83db4bec3c477d64142b289fbaa80ce0b83b59
Lorena Gil <67319308+logiflo@users.noreply.github.com>
2023-05-26 10:18:13 +0100

    Create pairing function and add more group routes

--- src/controller/groupController.js
+++ src/controller/groupController.js
@@ -1,19 +1,21 @@
-const { User, Group } = require('../../db');
+const { Group } = require('../../db');
+const { findUser } = require('../middleware/userUtils');
+const randomPairing = require('../utils/randomPairing');
 
 const groupController = {
   create: async (req, res) => {
     try {
-      const user = await User.findOne({
-        where: {
-          id: req.user.id,
-        },
-      });
+      const user = await findUser(req.user.id);
       if (user) {
         const {
           name, maxPrice, location, date, members,
         } = req.body;
 
-        const group = await Group.create({
+        const pairs = randomPairing(members);
+
+        global.logger.info(pairs);
+
+        const group = await user.create({
           name,
           maxPrice,
           location,
@@ -32,6 +34,84 @@ const groupController = {
     } catch (error) {
       global.logger.error(error.message);
       res.status(500).send({ message: 'Failed to create group' });
+    }
+  },
+
+  update: async (req, res) => {
+    try {
+      const user = await findUser(req.user.id);
+      if (user) {
+        const { groupId } = req.params;
+        const {
+          name, maxPrice, location, date, members,
+        } = req.body;
+
+        const group = await Group.findOne({
+          where: {
+            id: groupId,
+          },
+        });
+
+        if (user.id === group.adminId) {
+          const updatedGroup = await group.update({
+            name,
+            maxPrice,
+            location,
+            date,
+          });
+
+          const existingMembers = await group.getMembers();
+          await Promise.all(existingMembers.map(async (existingMember) => {
+            await existingMember.destroy();
+          }));
+
+          const pairs = randomPairing(members);
+
+          global.logger.info(pairs);
+
+          await Promise.all(members.map(async (member) => {
+            await updatedGroup.createMember(member);
+          }));
+
+          return res.status(200).send(updatedGroup);
+        }
+        return res.status(403).send({ message: 'User is not the group admin' });
+      }
+      return res.status(404).send({ message: 'User not logged' });
+    } catch (error) {
+      global.logger.error(error.message);
+      res.status(500).send({ message: 'Failed to update group' });
+    }
+  },
+
+  delete: async (req, res) => {
+    try {
+      const user = await findUser(req.user.id);
+      if (user) {
+        const { groupId } = req.params;
+
+        const group = await Group.findOne({
+          where: {
+            id: groupId,
+          },
+        });
+
+        if (user.id === group.adminId) {
+          const existingMembers = await group.getMembers();
+          await Promise.all(existingMembers.map(async (existingMember) => {
+            await existingMember.destroy();
+          }));
+
+          await group.destroy();
+
+          return res.status(200).send({ message: 'Group deleted successfuly' });
+        }
+        return res.status(403).send({ message: 'User is not the group admin' });
+      }
+      return res.status(404).send({ message: 'User not logged' });
+    } catch (error) {
+      global.logger.error(error.message);
+      res.status(500).send({ message: 'Failed to delete group' });
     }
   },
 };
